157f003cc6f52582b121a418684b8657cb13c3c9
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-11 01:01:27 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-const DBConnection = async () => {
+const DBConnection = async () => {   
     const MONGODB_URL = `mongodb://harshal:endisthebeginning@ac-uasy5vv-shard-00-00.9rsz1gu.mongodb.net:27017,ac-uasy5vv-shard-00-01.9rsz1gu.mongodb.net:27017,ac-uasy5vv-shard-00-02.9rsz1gu.mongodb.net:27017/?ssl=true&replicaSet=atlas-qu4pdd-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
